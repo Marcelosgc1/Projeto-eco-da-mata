5749f5405de86dcd52859310b1c8ec3162d61521
Matheus Felipe <matheusfelipesoares61@gmail.com>
2024-05-16 13:30:59 -0300

    Iniciando o modelo das avaliações

--- eco_da_mata_app/reviews/models.py
+++ eco_da_mata_app/reviews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    person_name = CharField(max_length=60)
+    phrase = TextField()
+    class Grade(models.IntegerChoices):
+        _1_STAR = 1, 'Very bad'
+        _2_STAR = 2, 'Bad'
+        _3_STAR = 3, 'Okay'
+        _4_STAR = 4, 'Good'
+        _5_STAR = 5, 'Very good'
+    
